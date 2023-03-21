65d144499d3c147834d8b58df4a5413f0f51b7f8
burakcoskuns <burak_coskun@msn.com>
2023-03-21 07:49:24 -0700

    s
    s

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,26 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, We are <span className="purple">BPC Altyapı&Otomasyon LTD.STI. </span>
+           from <span className="purple"> Istanbul, Türkiye.</span>
+            <br /> As BPC, The advancement of technology and increasing demands from businesses require processes to be made more efficient, faster, and error-free. To achieve this, it is important to select the most suitable technologies and develop automation software on top of these technologies.
+
+These technologies and software help to perform processes more quickly and accurately, reducing the need for human involvement and preventing process errors. For example, an automation software can be used for an accounting process. This software enables automatic closure of accounts, report generation, and other operations. This reduces the possibility of human errors and ensures that processes are completed more quickly and efficiently. Similarly, an automotive manufacturer can automate sales and distribution processes. For instance, sales and distribution data can be stored in a database and reports can be automatically generated. This reduces the need for human involvement and lowers the probability of process errors.
+
+In conclusion, utilizing the most suitable technologies and automation software helps businesses to make their processes more efficient, faster, and error-free. This increases the efficiency and profitability of the business, providing a competitive advantage
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, some other activities that we love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Installation of applied visual products
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Infrastructure support
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Implementation of applied projects such as commissioning
             </li>
           </ul>
 
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,19 +16,19 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="purple"> INTRODUCE </span> OURSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              We fell in love with programming and We have at least learnt
+              something, We hope.. 🤷‍♂️
               <br />
-              <br />I am fluent in classics like
+              <br />We are fluent in classics like
               <i>
                 <b className="purple"> C++, Javascript and Python. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              Our field of Interest's are building new &nbsp;
               <i>
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
@@ -38,7 +38,7 @@ function Home2() {
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
+              Whenever possible, We also apply my passion for developing products
               with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
@@ -60,49 +60,25 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1>FIND US ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">contact</span> us
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
+                  href="tel:123-456-7890"
                   className="icon-colour  home-social-icons"
                 >
-                  <AiFillGithub />
+                  <AiFillPhone />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
+                  href="mailto:info@example.com"
                   className="icon-colour  home-social-icons"
                 >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
+                  <AiFillMail />
                 </a>
               </li>
             </ul>
@@ -112,4 +88,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
